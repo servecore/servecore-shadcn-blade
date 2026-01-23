894bba2188b246ed838e2a03dc043bdae4c4bc3e
servecore <phpserve@outlook.com>
2026-01-23 15:26:03 +0700

    Fix: Globally share theme variable to prevent undefined variable error

--- src/ServeCoreServiceProvider.php
+++ src/ServeCoreServiceProvider.php
@@ -9,6 +9,9 @@ class ServeCoreServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        // Share theme variable globally
+        \Illuminate\Support\Facades\View::share('theme', 'default');
+
         if ($this->app->runningInConsole()) {
             $this->commands([
                 InstallCommand::class,
